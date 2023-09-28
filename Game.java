7faa904882662e6882d431379197279c3228ca2c
chloec435 <144473427+chloec435@users.noreply.github.com>
2023-09-28 16:29:32 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Game {
+  private Fishing fishing;
+  private Player player;
+  public void playerInfo() {
+    System.out.println("Enter a user name:");
+    
+  }
+}
