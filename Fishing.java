8ebd5af079baff35d5fa9496242ca169377f522e
chloec435 <144473427+chloec435@users.noreply.github.com>
2023-10-04 17:59:35 -0400

    Update Fishing.java

--- Fishing.java
+++ Fishing.java
@@ -1,30 +1,28 @@
 public class Fishing {
   private int totalFish;
-  private int gold;
-  // public Fishing(){
-  //   totalFish = 0;
-  //   gold = 0;
-  //   catchFish();
-  //   fledAway();
-  //   sellFish();
-  // }
+  private int totalGold;
   public void catchFish(int caught) {
     totalFish += caught;
     System.out.println("You have caught " + caught + " fish.");
   }
-  public void fledAway(int fled) {
-    totalFish -= fled;
-    System.out.println(fled + " fish have fled away.");
-  }
+  // public void fledAway(int fled) {
+  //   totalFish -= fled;
+  //   System.out.println(fled + " fish have fled away.");
+  //}
   public void sellFish(int fish){
-    totalFish -= fish;
-    gold = fish*5;
-    System.out.println("You have sold " + fish + " fish and received " + gold + ".");
+    if ((fish <= totalFish) && (fish >= 0)) {
+      totalFish -= fish;
+      int addGold = fish*100;
+      totalGold += addGold;
+      System.out.println("You have sold " + fish + " fish and received " + addGold + " gold.");
+    } else {
+      System.out.println("You don't have enough fish to sell.");
+    }
   }
   public int numFish() {
     return totalFish;
   }
   public int getGold() {
-    return gold;
+    return totalGold;
   }
 }
