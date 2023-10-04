28b3652633c90188a3159e0e6f06d8e809b5df9f
chloec435 <144473427+chloec435@users.noreply.github.com>
2023-10-04 17:59:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,7 @@
 public class Player {
   private String playerName;
-  private int playerGold;
-  private int playerFish;
+  // private int playerGold;
+  // private int playerFish;
   public Player(String name) {
     playerName = name;
   }
@@ -11,16 +11,16 @@ public class Player {
   public String getName() {
     return playerName;
   }
-  public void addGold(int gold) {
-    playerGold += gold;
-  }
-  public int checkGold() {
-    return playerGold;
-  }
-  public void addFish(int fish) {
-    playerFish += fish;
-  }
-  public int checkFish() {
-    return playerFish;
-  }
+//   public void addGold(int gold) {
+//     playerGold += gold;
+//   }
+//   public int checkGold() {
+//     return playerGold;
+//   }
+//   public void addFish(int fish) {
+//     playerFish += fish;
+//   }
+//   public int checkFish() {
+//     return playerFish;
+//   }
 }
