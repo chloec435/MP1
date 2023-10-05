ea7c6ee0ea7103aee812570b44c259d0c9ceee9b
chloec435 <144473427+chloec435@users.noreply.github.com>
2023-10-04 22:19:56 -0400

    Update CoinFlip.java

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,28 +1,35 @@
-import java.util.Scanner;
-public class CoinFlip {
-  private int headsOrTails;
-  private int coin;
-  public void flip(String HorT) {
-    int max = 1;
-    int min = 0;
-    int range = max - min + 1;
-    int bet = (int)(Math.random() * range) + min;
-    coin = bet;
-    if (HorT.equals("heads") || HorT.equals("h")) {
-      headsOrTails = 0;
-    } else if (HorT.equals("tails") || HorT.equals("t")) {
-      headsOrTails = 1;
-    }
-    if (headsOrTails = bet) {
-      System.out.println("Congrats! You have guessed right!");
-    } else {
-      System.out.println("Sorry, better luck next time!");
-    }
-  }
-  public int headsOrTails() {
-    return headsOrTails;
-  }
-  public int getCoin() {
-    return ht;
-  }
-}
+// import java.util.Scanner;
+// public class CoinFlip {
+//   private int headsOrTails;
+//   private int coin;
+//   private Fishing gfcf;
+//   public void flip(String HorT) {
+//     Scanner one = new Scanner(System.in);
+//     System.out.println("How much do you want to bet?");
+//     int wager = one.nextInt();
+//     gfcf = new Fishing();
+//     int max = 1;
+//     int min = 0;
+//     int range = max - min + 1;
+//     int bet = (int)(Math.random() * range) + min;
+//     coin = bet;
+//     if (HorT.equals("heads") || HorT.equals("h")) {
+//       headsOrTails = 0;
+//     } else if (HorT.equals("tails") || HorT.equals("t")) {
+//       headsOrTails = 1;
+//     }
+//     if (headsOrTails == bet) {
+//       System.out.println("Congrats! You have guessed right!");
+//       gfcf.addGoldFromCF(wager);
+//     } else {
+//       System.out.println("Sorry, better luck next time!");
+//       gfcf.addGoldFromCF(wager);
+//     }
+//   }
+//   public int headsOrTails() {
+//     return headsOrTails;
+//   }
+//   public int getCoin() {
+//     return coin;
+//   }
+// }
