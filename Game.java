baca91512e13f6aae524fe21a2cd37fe9cbf788a
chloec435 <144473427+chloec435@users.noreply.github.com>
2023-10-04 22:19:35 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Game {
   private Fishing fishing;
   private Player player;
-  private CoinFlip coinflip;
+  private int bet;
+  // private CoinFlip coinflip;
   public Game() {
     fishing = new Fishing();
     playerInfo();
@@ -11,9 +12,8 @@ public class Game {
       Scanner a = new Scanner(System.in);
       command = a.nextLine();
       commands(command);
-      if (command.equals("end")) {
-      break;
-      }
+      if (command.equals("coinflip")) { coinFlip(); }
+      if (command.equals("end")) { break; }
     }
   }
   
@@ -21,7 +21,7 @@ public class Game {
     System.out.println("Enter a user name:");
     Scanner b = new Scanner(System.in);
     player = new Player(b.nextLine());
-    System.out.println("Hello " + player.getName() + ". \nType \"catch\" to catch some fish. \nType \"sell\" to sell fish. \nType \"fish\" to check how much fish you have. \nType \"gold\" to view your balance. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
+    System.out.println("Hello " + player.getName() + ". \nType \"catch\" to catch some fish. \nType \"sell\" to sell fish. \nType \"fish\" to check how much fish you have. \nType \"gold\" to view your balance. \nType \"coinflip\" to bet some gold. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
   }
   
   public void commands(String command) {
@@ -49,12 +49,15 @@ public class Game {
       
     } else if (command.equals("gold")) {
       System.out.println("You have " + fishing.getGold() + " gold.");
+
+    } else if (command.equals("coinflip")) {
+      System.out.println("Heads or tails?");
       
     } else if (command.equals("end")) {
       System.out.println("The game has ended.");
       
     } else if (command.equals("command")) {
-        System.out.println("Type \"catch\" to catch some fish. \nType \"sell\" to sell fish. \nType \"fish\" to check how much fish you have. \nType \"gold\" to view your balance. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
+        System.out.println("Type \"catch\" to catch some fish. \nType \"sell\" to sell fish. \nType \"fish\" to check how much fish you have. \nType \"gold\" to view your balance. \nType \"coinflip\" to bet some gold. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
       
     } else {
       System.out.println("Please try again.");
@@ -62,9 +65,20 @@ public class Game {
   }
 
   public void coinFlip() {
-    System.out.println("Heads or tails?");
+    // coinflip = new CoinFlip();
     Scanner e = new Scanner(System.in);
-    coinflip.flip(e.nextLine());
-    
+    String HorT = e.nextLine();
+    int max = 1;
+    int min = 0;
+    int range = max - min + 1;
+    int coinSide = (int)(Math.random() * range) + min;
+    if (HorT.equals("heads") || HorT.equals("h")) {
+       bet = 0;
+     } else if (HorT.equals("tails") || HorT.equals("t")) {
+       bet = 1;
+     }
+    Scanner f = new Scanner(System.in);
+    System.out.println("How much gold do you want to bet?");
+    fishing.addGoldFromCF(bet, f.nextInt(), coinSide);
   }
 }
