438dfef9bca1e3309c4d38d8beedbddba24f2771
chloec435 <144473427+chloec435@users.noreply.github.com>
2023-10-30 12:20:35 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -16,8 +16,8 @@ public class Game {
       scanner = new Scanner(System.in);
       command = scanner.nextLine();
       commands(command);
-      if (command.equalsIgnoreCase("coinflip")) coinFlip();
-      if (command.equalsIgnoreCase("blackjack")) blackJack();
+      if (command.equalsIgnoreCase("coinflip") || command.equalsIgnoreCase("cf")) coinFlip();
+      if (command.equalsIgnoreCase("blackjack") || command.equalsIgnoreCase("bj")) blackJack();
       if (command.equalsIgnoreCase("end")) break;
     }
   }
@@ -26,16 +26,16 @@ public class Game {
     System.out.println("Enter a user name:");
     Scanner b = new Scanner(System.in);
     String playerName = b.nextLine();
-    System.out.println("Hello " + playerName + ". \nType \"catch\" to catch some fish. \nType \"sell\" to sell fish. \nType \"fish\" to check how much fish you have. \nType \"gold\" to view your balance. \nType \"coinflip\" to bet gold on heads or tails. \nType \"blackjack\" to play a round of blackjack. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
+    System.out.println("Hello " + playerName + ". \nType \"catch\" or \"c\" to catch some fish. \nType \"sell\" or \"s\" to sell fish. \nType \"fish\" or \"f\" to check how much fish you have. \nType \"gold\" or \"g\" to view your balance. \nType \"coinflip\" or \"cf\" to bet gold on heads or tails. \nType \"blackjack\" or \"bj\" to play a round of blackjack. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
   }
   
   public void commands(String command) {
     int max = 10;
     int min = 1;
     final int range = max - min + 1;
-    if (command.equalsIgnoreCase("catch")) {
+    if (command.equalsIgnoreCase("catch") || command.equalsIgnoreCase("c")) {
       fishing.catchFish((int)(Math.random() * range) + min);
-    } else if (command.equalsIgnoreCase("sell")) {
+    } else if (command.equalsIgnoreCase("sell") || command.equalsIgnoreCase("s")) {
       System.out.println("How much fish do you want to sell?");
       try {
         Scanner c = new Scanner(System.in);
@@ -47,18 +47,18 @@ public class Game {
         int newSell = d.nextInt();
         fishing.sellFish(newSell);
       }
-    } else if (command.equalsIgnoreCase("fish")) {
+    } else if (command.equalsIgnoreCase("fish") || command.equalsIgnoreCase("f")) {
       System.out.println("You have a total of " + fishing.numFish() + " fish.");
-    } else if (command.equalsIgnoreCase("gold")) {
+    } else if (command.equalsIgnoreCase("gold") || command.equalsIgnoreCase("g")) {
       System.out.println("You have " + fishing.getGold() + " gold.");
-    } else if (command.equalsIgnoreCase("coinflip")) {
+    } else if (command.equalsIgnoreCase("coinflip") || command.equalsIgnoreCase("cf")) {
       System.out.println("How much gold do you want to bet?");
-    } else if (command.equalsIgnoreCase("blackjack")) {
+    } else if (command.equalsIgnoreCase("blackjack") || command.equalsIgnoreCase("bj")) {
       System.out.println("How much gold would you like to bet?");
     } else if (command.equalsIgnoreCase("end")) {
       System.out.println("The game has ended.");
-    } else if (command.equalsIgnoreCase("command")) {
-        System.out.println("Type \"catch\" to catch some fish. \nType \"sell\" to sell fish. \nType \"fish\" to check how much fish you have. \nType \"gold\" to view your balance. \nType \"coinflip\" to bet some gold. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
+    } else if (command.equalsIgnoreCase("command") || command.equalsIgnoreCase("commands")) {
+        System.out.println("Type \"catch\" or \"c\" to catch some fish. \nType \"sell\" or \"s\" to sell fish. \nType \"fish\" or \"f\" to check how much fish you have. \nType \"gold\" or \"g\" to view your balance. \nType \"coinflip\" or \"cf\" to bet gold on heads or tails. \nType \"blackjack\" or \"bj\" to play a round of blackjack. \nType \"command\" to view the commands. \nType \"end\" to end this game.");
     } else {
       System.out.println("Please try again.");
     }
@@ -70,13 +70,14 @@ public class Game {
     if (fishing.getGold() < gold) {
       System.out.println("You do not have enough gold. Type \"catch\" to catch some fish or \"sell\" to sell fish for gold!");
     } else {
-      // if (Coinflip.getWinStreak() == 0) {
-      //   coinflip = new Coinflip("start");
-      //   fishing.addGoldFromCF(coinflip.winOrLose(), gold);
-      // } else {
+      if (Coinflip.getWinStreak() == 0) {
+        coinflip = new Coinflip("start");
+        fishing.addGold(coinflip.winOrLose(), gold);
+        coinflip.winStreak(1);
+      } else {
         coinflip = new Coinflip(coinflip.getWinStreak()+1);
         fishing.addGold(coinflip.winOrLose(), gold, coinflip.getWinStreak());
-      // }
+      }
     }
   }
 
@@ -88,8 +89,7 @@ public class Game {
     } else {
     blackjack = new Blackjack();
     if (blackjack.getPlayerWin()) fishing.addGold(bjBet);
-    else if (!blackjack.getPlayerWin()) fishing.removeGold(bjBet);
-    else System.out.println("No gold was lost or gained.");
+    else if (!blackjack.getPlayerWin() && !(blackjack.getPlayerTotal() == blackjack.getDealerTotal())) fishing.removeGold(bjBet);
     }
   }
 }
